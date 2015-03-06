d515054e4afa3ed214b894712f0d5daea04b7491
vinivendra <vinivendra@gmail.com>
2015-03-06 10:11:07 -0300

    Created the project.

---
+++ Project/Project/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Vinicius Vendramini on 06/03/15.
+//  Copyright (c) 2015 Vinicius Vendramini. All rights reserved.
+//
+
+import UIKit
+
+import JavaScriptCore
+
+class ViewController: UIViewController {
+
+    let context = JSContext()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
