f417ea6bc457f1b759541a3b675c21a98e87509b
vinivendra <vinivendra@gmail.com>
2015-03-16 15:07:33 -0300

    Created test methods for Color.

--- Project - ObjC/Project - ObjCTests/ColorTests.m
+++ Project - ObjC/Project - ObjCTests/ColorTests.m
@@ -1,19 +1,67 @@
 
-// TODO: Create tests for the current color mehtods
 // TODO: Create initializers for array, dict, etc
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+
 @interface ColorTests : XCTestCase
 
 @end
 
+
+static NSArray *testColors;
+
+
 @implementation ColorTests
 
-- (void)testExample {
+- (void)setUp {
+    testColors = @[
+        [Color colorWithRed:0.3 green:0.3 blue:0.3 alpha:1.0],
+        [Color colorWithRed:0.4 green:0.0 blue:1.0 alpha:0.0],
+        [Color colorWithRed:0.2 green:0.3 blue:0.2 alpha:0.4]
+    ];
+}
 
-    XCTAssert(YES, @"Pass");
+- (void)testColorWithCArray {
+
+    // With
+    CGFloat expectedColors[3][4]
+        = {{0.3, 0.3, 0.3, 1.0}, {0.4, 0.0, 1.0, 0.0}, {0.2, 0.3, 0.2, 0.4}};
+
+    // When
+    NSArray *resultColors = @[
+        [Color colorWithCArray:expectedColors[0]],
+        [Color colorWithCArray:expectedColors[1]],
+        [Color colorWithCArray:expectedColors[2]]
+    ];
+
+    // Then
+    for (int i = 0; i < testColors.count; i++) {
+        XCTAssertEqualObjects(testColors[i], resultColors[i]);
+    }
+}
+
+- (void)testColorTimes {
+
+    // With
+    NSArray *expectedColors = @[
+        [Color colorWithRed:0.3 * 0.1 green:0.3 * 0.1 blue:0.3 * 0.1 alpha:1.0],
+        [Color colorWithRed:0.4 * 1.2 green:0.0 * 1.2 blue:1.0 * 1.2 alpha:0.0],
+        [Color colorWithRed:-0.2 green:-0.3 blue:-0.2 alpha:0.4]
+    ];
+
+    // When
+    NSArray *resultColors = @[
+                              [((UIColor *)testColors[0])times:0.1],
+                              [((UIColor *)testColors[1])times:1.2],
+                              [((UIColor *)testColors[2])times:-1.0]
+                              ];
+    
+    // Then
+    for (int i = 0; i < testColors.count; i++) {
+        XCTAssertEqualObjects(expectedColors[i], resultColors[i]);
+    }
 }
 
 @end
