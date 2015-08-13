d43661c0b6c063b28ebb866c13c5fecea26ace06
vinivendra <vinivendra@gmail.com>
2015-08-13 20:00:36 -0300

    Added a collectionView with cells

--- EngineKitCreator/EngineKitCreator/ObjectsMenuController.swift
+++ EngineKitCreator/EngineKitCreator/ObjectsMenuController.swift
@@ -3,29 +3,72 @@
 import UIKit
 
 
+let id = "cellID"
+
+
 protocol MenuController {
     func setupMenuView(menuView: MenuView)
 }
 
 
 class ObjectsMenuController: NSObject,
-                             UICollectionViewDelegate,
-                             UICollectionViewDataSource,
-                             MenuController {
+    UICollectionViewDelegate,
+    UICollectionViewDataSource,
+    UICollectionViewDelegateFlowLayout,
+MenuController {
 
-    func collectionView(collectionView: UICollectionView,
-                        numberOfItemsInSection section: Int) -> Int {
-        return 0
+    var collectionView: UICollectionView?
+
+    // MARK: - UICollectionViewDataSource
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
     }
 
-    func collectionView(collectionView: UICollectionView,
-                        cellForItemAtIndexPath indexPath: NSIndexPath)
-                        -> UICollectionViewCell {
-        return UICollectionViewCell()
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
     }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell
+        cell = collectionView.dequeueReusableCellWithReuseIdentifier(id,
+            forIndexPath: indexPath)
+            as! UICollectionViewCell
+
+        cell.backgroundColor = UIColor.redColor()
+
+        return cell
+    }
+
+    // MARK: - UICollectionViewDelegateFlowLayout
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(100, 100)
+    }
+
+    // MARK: - MenuController
 
     func setupMenuView(menuView: MenuView) {
         menuView.backgroundColor = UIColor.yellowColor()
-    }
 
+        let size = menuView.frame.size
+        let frame = CGRectMake(0, 0, size.width, size.height)
+        let layout = UICollectionViewFlowLayout()
+
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+
+        collectionView = UICollectionView(frame: frame,
+            collectionViewLayout: layout)
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        collectionView?.backgroundColor = UIColor.greenColor()
+
+        collectionView?.registerClass(UICollectionViewCell.self,
+            forCellWithReuseIdentifier: id)
+        
+        menuView.addSubview(collectionView!)
+    }
+    
 }
+
