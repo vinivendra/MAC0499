3a81fc3b205753c0da76919075584e1ea8301e41
vinivendra <vinivendra@gmail.com>
2015-08-13 07:15:06 -0300

    A few swift translations

--- EngineKit/EngineKit/Math/Vector.swift
+++ EngineKit/EngineKit/Math/Vector.swift
@@ -7,9 +7,16 @@ import SceneKit
 let originVector = Vector(x:0, y:0, z:0)
 
 
-public class Vector {
+public class Vector: DebugPrintable {
 
     let vector: SCNVector3
+
+    public var debugDescription: String {
+        get {
+            return String(format: "(x = %lf, y = %lf, z = %lf)",
+                          arguments: [vector.x, vector.y, vector.z])
+        }
+    }
 
     public var x: Float { get { return vector.x } }
     public var y: Float { get { return vector.y } }
@@ -77,8 +84,89 @@ public class Vector {
         self.init(x:x, y:y, z:z)
     }
 
+    public convenience init(dictionary: [String : Float]) {
+        var x: Float = 0.0
+        var y: Float = 0.0
+        var z: Float = 0.0
+
+        if let result = dictionary["x"] {
+            x = result
+        } else if let result = dictionary["X"] {
+            x = result
+        } else if let result = dictionary["0"] {
+            x = result
+        }
+
+        if let result = dictionary["y"] {
+            y = result
+        } else if let result = dictionary["Y"] {
+            y = result
+        } else if let result = dictionary["1"] {
+            y = result
+        }
+
+        if let result = dictionary["z"] {
+            z = result
+        } else if let result = dictionary["Z"] {
+            z = result
+        } else if let result = dictionary["2"] {
+            z = result
+        }
+
+        self.init(x:x, y:y, z:z)
+    }
+
+    public convenience init(string: String) {
+        let scanner = NSScanner(string: string)
+        let numbers = NSCharacterSet(charactersInString: "-0123456789.")
+        var array: [Float] = []
+
+        var done = false
+        var contents = 0
+
+        while (!done && contents != 3) {
+            var number: NSString? = ""
+
+            scanner.scanUpToCharactersFromSet(numbers, intoString: nil)
+
+            done = !(scanner.scanCharactersFromSet(numbers, intoString: &number))
+
+            if let float = number?.floatValue {
+                array.append(float)
+                contents++
+            }
+        }
+
+        self.init(array:array)
+    }
+
+    public convenience init(vector: Vector) {
+        self.init(x:vector.x, y:vector.y, z:vector.z)
+    }
+
+    public convenience init(object: Any) {
+        if let point = object as? CGPoint {
+            self.init(point: point)
+        }
+        else if let float = object as? Float {
+            self.init(number: float)
+        }
+        else if let array = object as? [Float] {
+            self.init(array: array)
+        }
+        else if let dictionary = object as? [String: Float] {
+            self.init(dictionary: dictionary)
+        }
+        else if let string = object as? String {
+            self.init(string: string)
+        }
+        else {
+            self.init(x:0, y:0, z:0)
+        }
+    }
+
 //------------------------------------------------------------------------------
-// MARK: - Comparing objects
+// MARK: - Common overrides
 //------------------------------------------------------------------------------
 
     public func isEqual(object: Any) -> Bool {
@@ -88,8 +176,27 @@ public class Vector {
         return false
     }
 
+    public func isApproximatelyEqual(object: Any) -> Bool {
+        let margin: Float = 0.00001
 
+        if let otherVector = object as? Vector {
+            let other = otherVector.vector
+            if (Float.abs(vector.x - other.x) > Float.abs((vector.x * margin) + margin)) {
+                return false
+            }
+            if (Float.abs(vector.y - other.y) > Float.abs((vector.y * margin) + margin)) {
+                return false
+            }
+            if (Float.abs(vector.z - other.z) > Float.abs((vector.z * margin) + margin)) {
+                return false
+            }
+        }
+        return true
+    }
 }
+
+infix operator ==~ {associativity left precedence 130}
+infix operator !=~ {associativity left precedence 130}
 
 public func == (left: Vector, right:Vector) -> Bool {
     return left.isEqual(right)
@@ -97,4 +204,12 @@ public func == (left: Vector, right:Vector) -> Bool {
 
 public func != (left: Vector, right:Vector) -> Bool {
     return !left.isEqual(right)
+}
+
+public func ==~ (left: Vector, right:Vector) -> Bool {
+    return left.isApproximatelyEqual(right)
+}
+
+public func !=~ (left: Vector, right:Vector) -> Bool {
+    return !left.isApproximatelyEqual(right)
 }
--- EngineKit/EngineKitTests/Math/VectorTests.swift
+++ EngineKit/EngineKitTests/Math/VectorTests.swift
@@ -72,6 +72,28 @@ class VectorTests: XCTestCase {
         }
     }
 
+    func testIsApproximatelyEqual() {
+
+        for (var i = 0; i < standardArrays.count; i++) {
+            // Bijection
+            let vectorArray = standardArrays[i]
+            let nextVectorArray = standardArrays[(i+1) % standardArrays.count]
+
+            let vector1 = Vector(x: vectorArray[0],
+                y: vectorArray[1],
+                z: vectorArray[2])
+            let vector2 = Vector(x: vectorArray[0],
+                y: vectorArray[1],
+                z: vectorArray[2])
+            let vector3 = Vector(x: nextVectorArray[0],
+                y: nextVectorArray[1],
+                z: nextVectorArray[2])
+
+            XCTAssert(vector1 ==~ vector2)
+            XCTAssert(vector1 !=~ vector3)
+        }
+    }
+
 //------------------------------------------------------------------------------
 // MARK: Getting the Vector's information
 //------------------------------------------------------------------------------
@@ -173,7 +195,7 @@ class VectorTests: XCTestCase {
     func testInitWithCGPoint() {
         for (let array) in standardArrays {
             // Gold Standard
-            var point = CGPointMake(CGFloat(array[0]), CGFloat(array[1]))
+            let point = CGPointMake(CGFloat(array[0]), CGFloat(array[1]))
             let standard = Vector(x: Float(point.x), y: Float(-point.y), z: 0)
 
             // Actual Result
@@ -212,6 +234,120 @@ class VectorTests: XCTestCase {
         }
     }
 
+    func testInitWithDictionary() {
+        for (var i = 0; i < standardArrays.count; i++) {
+            // Gold Standard
+            let standard = standardVectors[i]
+
+            // Actual Result
+            let dictionary1 = ["x": standard.x, "y": standard.y, "z":standard.z];
+            let dictionary2 = ["X": standard.x, "Y": standard.y, "Z":standard.z];
+            let dictionary3 = ["0": standard.x, "1": standard.y, "2":standard.z];
+            let result1 = Vector(dictionary:dictionary1)
+            let result2 = Vector(dictionary:dictionary2)
+            let result3 = Vector(dictionary:dictionary3)
+
+            // Comparison
+            XCTAssert(standard == result1)
+            XCTAssert(standard == result2)
+            XCTAssert(standard == result3)
+        }
+    }
+
+    func testInitString() {
+        for (var i = 0; i < standardArrays.count; i++) {
+            // Gold Standard
+            let standard = standardVectors[i]
+
+            // Actual Result
+            let format1 = "[%lf, %lf, %lf]"
+            let format2 = "vn %lf %lf %lf"
+            let format3 = "(%lf %lf %lf)"
+            let string1 = String(format: format1, arguments: [standard.x, standard.y, standard.z])
+            let string2 = String(format: format2, arguments: [standard.x, standard.y, standard.z])
+            let string3 = String(format: format3, arguments: [standard.x, standard.y, standard.z])
+            let result1 = Vector(string: string1)
+            let result2 = Vector(string: string2)
+            let result3 = Vector(string: string3)
+
+            // Comparison
+            XCTAssert(standard ==~ result1)
+            XCTAssert(standard ==~ result2)
+            XCTAssert(standard ==~ result3)
+        }
+    }
+
+    func testInitWithVector() {
+        for (var i = 0; i < standardArrays.count; i++) {
+            // Gold Standard
+            let standard = standardVectors[i]
+
+            // Actual Result
+            let result = Vector(vector:standard)
+
+            // Comparison
+            XCTAssert(standard == result)
+        }
+    }
+
+    func testInitWithObject() {
+        for (var i = 0; i < standardArrays.count; i++) {
+            // Gold Standard
+            let standard = standardArrays[i]
+            var standards: [Vector] = []
+
+            let point = CGPointMake(CGFloat(standard[0]), CGFloat(standard[1]))
+            standards.append(Vector(point: point))
+
+            standards.append(Vector(number: standard[0]))
+            standards.append(Vector(number: standard[1]))
+            standards.append(Vector(number: standard[2]))
+
+            standards.append(Vector(array: standard))
+
+            let dictionary1 = ["x": standard[0], "y": standard[1], "z":standard[2]];
+            let dictionary2 = ["X": standard[0], "Y": standard[1], "Z":standard[2]];
+            let dictionary3 = ["0": standard[0], "1": standard[1], "2":standard[2]];
+            standards.append(Vector(dictionary:dictionary1))
+            standards.append(Vector(dictionary:dictionary2))
+            standards.append(Vector(dictionary:dictionary3))
+
+            let format1 = "[%lf, %lf, %lf]"
+            let format2 = "vn %lf %lf %lf"
+            let format3 = "(%lf %lf %lf)"
+            let string1 = String(format: format1, arguments: [standard[0], standard[1], standard[2]])
+            let string2 = String(format: format2, arguments: [standard[0], standard[1], standard[2]])
+            let string3 = String(format: format3, arguments: [standard[0], standard[1], standard[2]])
+            standards.append(Vector(string: string1))
+            standards.append(Vector(string: string2))
+            standards.append(Vector(string: string3))
+
+            // Actual Result
+
+            var results: [Vector] = []
+
+            results.append(Vector(object: point))
+
+            results.append(Vector(object: standard[0]))
+            results.append(Vector(object: standard[1]))
+            results.append(Vector(object: standard[2]))
+
+            results.append(Vector(object: standard))
+
+            results.append(Vector(object:dictionary1))
+            results.append(Vector(object:dictionary2))
+            results.append(Vector(object:dictionary3))
+
+            results.append(Vector(object: string1))
+            results.append(Vector(object: string2))
+            results.append(Vector(object: string3))
+
+            // Comparison
+            for (var i = 0; i < standards.count; i++) {
+                XCTAssert(standards[i] ==~ results[i])
+            }
+        }
+    }
 }
 
 
