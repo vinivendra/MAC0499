baa1667cc52439860e8881bceac85daf6d5da683
vinivendra <vinivendra@gmail.com>
2015-09-11 19:21:25 -0400

    Added a scrollView and auto layout to the properties menu

--- EngineKitCreator/EngineKitCreator/PropertiesMenuViewController.swift
+++ EngineKitCreator/EngineKitCreator/PropertiesMenuViewController.swift
@@ -14,6 +14,15 @@ func setItemPositionZ(item: Item, newValue: CGFloat) {
     item.position = Position(x: item.position.x, y: item.position.y, z: newValue)
 }
 
+func setItemScaleX(item: Item, newValue: CGFloat) {
+    item.scale = Vector(x: newValue, y: item.scale.y, z: item.scale.z)
+}
+func setItemScaleY(item: Item, newValue: CGFloat) {
+    item.scale = Vector(x: item.scale.x, y: newValue, z: item.scale.z)
+}
+func setItemScaleZ(item: Item, newValue: CGFloat) {
+    item.scale = Vector(x: item.scale.x, y: item.scale.y, z: newValue)
+}
 
 class PropertiesMenuViewController: UIViewController, MenuController, UITextFieldDelegate {
 
@@ -24,6 +33,10 @@ class PropertiesMenuViewController: UIViewController, MenuController, UITextFiel
     @IBOutlet weak var positionXTextField: UITextField!
     @IBOutlet weak var positionYTextField: UITextField!
     @IBOutlet weak var positionZTextField: UITextField!
+
+    @IBOutlet weak var scaleXTextField: UITextField!
+    @IBOutlet weak var scaleYTextField: UITextField!
+    @IBOutlet weak var scaleZTextField: UITextField!
 
     init(item: Item) {
         self.item = item
@@ -41,7 +54,10 @@ class PropertiesMenuViewController: UIViewController, MenuController, UITextFiel
 
         propertyActions = [positionXTextField : setItemPositionX,
             positionYTextField : setItemPositionY,
-            positionZTextField : setItemPositionZ]
+            positionZTextField : setItemPositionZ,
+            scaleXTextField : setItemScaleX,
+            scaleYTextField : setItemScaleY,
+            scaleZTextField : setItemScaleZ]
     }
 
     func textFieldShouldReturn(textField: UITextField) -> Bool {
